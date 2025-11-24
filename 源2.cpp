08176b742380a9e17787a5548e34e456fb666d11
TX88888 <1187629314@qq.com>
2025-11-24 19:45:42 +0800

    Add files via upload

---
+++ 源2.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	double r, h;
+//	const double PI = 3.1415926;
+//	cout << "请输入圆锥底面半径";
+//	cin >> r;
+//	cout << "请输入圆锥的高";
+//	cin >> h;
+//	double V = (1.0 / 3) * PI * r * r * h;
+//	cout << "圆锥的体积为" << V<< endl;
+//	return 0;
+//}
---
+++ 源9.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int a, b, d;
+//    char c;
+//    cin >> a >> c >> b;
+//    if (c == '/' && b == 0)
+//    {
+//        cout << "除数不能为0";
+//        
+//
+//    
+//    switch (c) {
+//    case '+': d = a + b; break;
+//    case '-': d = a - b; break;
+//    case '*': d = a * b; break;
+//    case '/': d = a / b; break;
+//    case '%': d = a % b; break;
+//    default:  cout << "运算符非法"; return 0; 
+//    }
+//
+//    cout << d;
+//    return 0;
+//}
